089cfeead8ac909a80c80e3439dc9e7de491aa3c
Matt Lachance <matt_lachance@hotmail.com>
2023-03-24 20:57:21 -0400

    Added some more logging

--- backup.sh
+++ backup.sh
@@ -17,6 +17,10 @@ log_file="$log_dir/${backup_type}_backup_id-${backup_id}_${timestamp}.log"
 
 mkdir -p "$log_dir"
 
+echo "$(date +%Y-%m-%d_%H:%M:%S) - Starting ${backup_type} Backup" >"$log_file"
+
+start_time=$(date +%s)
+
 if [ "$environment" = "development" ]; then
   case "$backup_type" in
     full)
@@ -49,4 +53,15 @@ else
   echo "Invalid environment: $environment" >&2
   echo "Valid environments are 'development' and 'production'" >&2
   exit 1
+fi
+
+end_time=$(date +%s)
+elapsed_time=$((end_time - start_time))
+
+if [ $elapsed_time -ge 3600 ]; then
+  elapsed_time_hours=$(echo "scale=2; $elapsed_time / 3600" | bc)
+  echo "$(date +%Y-%m-%d_%H:%M:%S) - ${backup_type} Backup completed in ${elapsed_time_hours} hours" >>"$log_file"
+else
+  elapsed_time_minutes=$(echo "scale=2; $elapsed_time / 60" | bc)
+  echo "$(date +%Y-%m-%d_%H:%M:%S) - ${backup_type} Backup completed in ${elapsed_time_minutes} minutes" >>"$log_file"
 fi
