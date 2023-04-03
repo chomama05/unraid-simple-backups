ab1166fc8514e768485f96a84edda11660e62b18
Matt Lachance <matt_lachance@hotmail.com>
2023-04-03 09:57:49 -0400

    Added missing reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const { Server: WebSocketServer } = require('ws');
 const { createCronJob, updateCronJob, deleteCronJob, loadCronJobsFromDatabase } = require('./cronManager');
-const { searchDirectories } = require('./helpers');
+const { searchDirectories, createNewDirectory } = require('./helpers');
 const {
   createBackupsTable,
   insertBackup,
