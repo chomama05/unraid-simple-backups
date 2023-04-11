f5a79325ca5f2fd65d29bf551ec68f257097ac1d
chomama05 <35979997+chomama05@users.noreply.github.com>
2023-04-11 10:10:08 -0400

    Removed cors lib

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const express = require('express');
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const sdk = require("vuetify-file-browser-server/sdk");
 const http = require('http');
